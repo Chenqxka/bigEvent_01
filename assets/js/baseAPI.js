85d1596bc24b387ba058284637122f0f4703720c
Chenqxka <278127524@qq.com>
2020-09-04 10:50:38 +0800

    user over

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,28 +1,3 @@
-// // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// // 会先调用 ajaxPrefilter 这个函数
-// // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-// $.ajaxPrefilter(function (options) {
-//   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-//   options.url = 'http://ajax.frontend.itheima.net' + options.url
-// })
-// // 统一为有权限的接口，设置headers请求头
-// if (options.url.indexOf('/my/') !== -1) {
-//   options.headers = {
-//     Authorization: localStorage.getItem('token') || ''
-//   }
-// }
-// // 当请求结束后，判断用户的设置访问权限
-// options.complete = function(res) {
-//   // 在complete 回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-//   if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
-//     // 强制清空 token 
-//     localStorage.getItem('token')
-//     // 强制跳转到登录页面
-//     location.href = '/login.html'
-//   }
-// }
-
-
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
